39a31bf52d77f10910304015834ae904d3c2211c
Batriftale <70813542+Batriftale@users.noreply.github.com>
2021-04-17 20:35:19 +0300

    Update task7.cpp

--- task7.cpp
+++ task7.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <ctime>
 #include <thread>
+#include <random>
 #include <algorithm>
 #include <execution>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
@@ -55,6 +57,43 @@ int main()
     auto end   = chrono::high_resolution_clock::now();
 
     cout << "time taken for sequenced : " << chrono::duration<float>(end - start).count() << " s" << endl;
+}
+//#1
+void Pi_search(int N, int Radius)
+{
+    int N_in = 0;
 
+    random_device random_device; 
+    mt19937 generator(random_device());
 
+    uniform_int_distribution<> distribution(-Radius, Radius); 
+
+    for (int i = 0; i < 2 * N; i++)
+    {
+        int x = distribution(generator);
+        int y = distribution(generator);
+        if ((x * x + y * y) <= Radius * Radius)
+        {
+            N_in++;
+        }
+    }
+    cout << 4 * double(N_in) / double(N) << endl; 
+}
+
+int main(int argc, char ** argv)
+{
+    int N = 1000000;
+    int n = thread::hardware_concurrency();
+    int Radius = 1;
+
+    vector<thread> threads;
+
+    int each = N/n;
+
+    for (size_t i = 0 ; i < n; i++)
+    {
+        threads.push_back(thread(Pi_search, each, Radius));
+
+    }
+    for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
 }
